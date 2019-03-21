a4c8fecbaf9f195a92c07b0800b24cb7dbf637de
Ryan Ray <ryan314r@gmail.com>
2019-03-21 13:46:28 -0500

    Add discord and slack to install deps

--- bin/install_deps.sh
+++ bin/install_deps.sh
@@ -1,6 +1,7 @@
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
+rm microsoft.gpg
 
 apt install -y apt-transport-https
 apt update
@@ -34,3 +35,10 @@ apt install -y \
 	network-manager
 
 wget -qO- https://get.docker.com/ | bash
+usermod -aG docker ryan
+
+snap install slack --classic
+
+wget "https://discordapp.com/api/download?platform=linux&format=deb" -O discord.deb
+apt install -y ./discord.deb
+rm discord.deb
